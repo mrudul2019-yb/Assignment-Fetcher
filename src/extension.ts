75b10b416912947b38ed84b5fbf7a785e3fafd41
mrudul2019 <mrudulmnair@gmail.com>
2021-07-26 04:42:00 +0530

    July 26, 2021 4:42 AM

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import { fetch } from './gitSet';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,8 +18,21 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from Assignment_Fetcher!');
 	});
-
 	context.subscriptions.push(disposable);
+	disposable = vscode.commands.registerCommand('assignment-fetcher.gitSet', async () => { 
+		try{
+			let Assignment = "Assignment_4";
+			vscode.window.showInformationMessage('Fetching....');
+			await fetch(Assignment); 
+			vscode.window.showInformationMessage('Successfully Fetched!');
+
+		}
+		catch(err){
+			vscode.window.showInformationMessage('Oops something Went Wrong!');
+		}
+	});
+	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated
