b8920767d5d9c6a86b6ebf77724c05b65669f1e2
mrudul2019 <mrudulmnair@gmail.com>
2021-07-29 11:50:06 +0530

    July 29, 2021 11:50 AM

--- src/gitSet.ts
+++ src/gitSet.ts
@@ -10,24 +10,16 @@ const options: Partial<SimpleGitOptions> = {
  };
 const git: SimpleGit = simpleGit(options);
 // const git: SimpleGit = simpleGit();
-// const USER = 'mrudul2019';
-// const PASS = "";
-// const REPO = 'github.com/mrudul2019/0026G.git';
 
 export async function fetch(name:string) {
     try{
         let LabRepo:(string|undefined) = vscode.workspace.getConfiguration().get('Lab Repository');
 
         console.log(`working for ${name}??`);
-        await git.checkout("master");
-        // // await git.clone("https://github.com/Tester-jr/Lab_Questions.git", "../Lab_Questions",  ['-b', name, '--single-branch']);
-        // // await git.addRemote('lab_question', '../Lab_Questions');
+        await git.checkout("main");
         await git.checkout(['-b', name]);
-        // // await git.pull('lab_question', name, ['--allow-unrelated-histories']);
         await git.pull(LabRepo, name, ['--allow-unrelated-histories']);
         await git.push('origin', name, ['-u']);
-        // await git.push(`https://${USER}:${PASS}@${REPO}`, name, ['-u']);
-        // await git.removeRemote('lab_question');
         console.log('worked');
 
     }
